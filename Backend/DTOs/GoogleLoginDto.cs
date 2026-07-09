43e295dbc7138b2b86c2c55f26308bf61c247aaa
Ahmad Al-Srdah <ahmad2315753@gmail.com>
2026-07-09 19:47:54 +0300

    Add google service

---
+++ Backend/DTOs/GoogleLoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TACT.DTOs;
+
+public class GoogleLoginDto
+{
+    public string IdToken { get; set; } = string.Empty;
+    public string AccessToken { get; set; } = string.Empty;
+    public string? RefreshToken { get; set; }
+    public int ExpiresInSeconds { get; set; } 
+}
