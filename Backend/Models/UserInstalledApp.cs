a63fc3480983c88218a814e8adf76a6df15d07af
Ahmad Al-Srdah <ahmad2315753@gmail.com>
2026-07-15 15:14:56 +0300

    Add UserInstalledApp model

---
+++ Backend/Models/UserInstalledApp.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TACT.Models;
+
+public class UserInstalledApp
+{
+    [Key]
+    public long Id { get; set; }
+
+    [Required]
+    public string? PackageName { get; set; } = string.Empty;
+
+    public string? AppName { get; set; } = string.Empty;
+
+    [Required]
+    public long UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; } = null!;
+    
+    public DateTime LastSyncedAt { get; set; } = DateTime.UtcNow;
+}
