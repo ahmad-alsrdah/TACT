6d7e29a0af0215b04927d681aaa0b280c177ffcd
Ahmad Al-Srdah <ahmad2315753@gmail.com>
2026-07-15 15:15:39 +0300

    Add DTOs

---
+++ Backend/DTOs/UserAppItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TACT.DTOs;
+
+public class UserAppItemDto
+{
+    [Required]
+    public string PackageName { get; set; } = string.Empty;
+
+    public string AppName { get; set; } = string.Empty;
+}
---
+++ Backend/DTOs/UserAppsSyncDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TACT.DTOs;
+
+public class UserAppsSyncDto
+{
+    [Required]
+    public List<UserAppItemDto> Apps { get; set; } = new();
+}
+
