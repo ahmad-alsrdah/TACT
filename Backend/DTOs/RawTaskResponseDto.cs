89132fde4a00db43c8c1786c5903fc60384ccd93
Ahmad Al-Srdah <ahmad2315753@gmail.com>
2026-07-23 20:17:04 +0300

    Add DTOs

---
+++ Backend/DTOs/RawTaskResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TACT.DTOs;
+
+public class RawTaskResponseDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string? StartTime { get; set; }
+    public string? EndTime { get; set; }
+    public List<string> BlockedPackageNames { get; set; } = new();
+}
---
+++ Backend/DTOs/ResponseWorkTaskDto.cs
@@ -0,0 +1,18 @@
+﻿using TACT.Enums;
+using TACT.Models;
+
+namespace TACT.DTOs;
+
+public class ResponseWorkTaskDto
+{
+    public long Id { get; set; }
+    
+    public string? Title { get; set; }
+    
+    public DateTime StartTime { get; set; } =  DateTime.UtcNow;
+    
+    public DateTime EndTime { get; set; }
+    
+    public List<ClientAppsDto> BlockedApps { get; set; } = new();
+    
+}
